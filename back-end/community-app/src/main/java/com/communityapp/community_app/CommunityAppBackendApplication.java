ac217024f6696667dc0eadfa350acfcc482e0c73
Victor Augusto <victorcosta939@gmail.com>
2024-10-18 21:39:32 -0300

    first backend

---
+++ back-end/community-app/src/main/java/com/communityapp/community_app/CommunityAppBackendApplication.java
@@ -0,0 +1,12 @@
+package com.communityapp.community_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityAppBackendApplication.class, args);
+    }
+}
---
+++ back-end/community-app/src/test/java/com/communityapp/community_app/CommunityAppBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package com.communityapp.community_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CommunityAppBackendApplication.class)
+public class CommunityAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
