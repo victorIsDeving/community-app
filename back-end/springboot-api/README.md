e9099523a9672ab8802d1c1b4907913852a5b075
Victor Augusto <victorcosta939@gmail.com>
2024-10-24 00:19:04 -0300

    configurando banco dados com springboot

---
+++ back-end/springboot-api/README.md
@@ -0,0 +1,11 @@
+### API REST para usar no Community
+
+Aqui é onde configuramos as APIs que chamam o banco de dados
+
+As tecnologias são JAVA 23 e Springboot com Banco de Dados PostgreSQL.
+Desenvolvidos usando o IntelliJ IDEA 2022
+
+Algumas fontes que usei para criar foram
+- [Vídeos de API REST com SPRINGBOOT](https://www.youtube.com/playlist?list=PL8iIphQOyG-D2FP9wkg12AavzmVRWEcnJ)
+- [PostgreSQL](https://hcode.com.br/blog/o-que-e-o-postgresql-instalando-e-criando-primeiro-banco-de-dados)
+- [Spring Initializer](https://start.spring.io/)
---
+++ back-end/springboot-api/src/main/java/com/community_backend/springboot_api/models/User.java
@@ -0,0 +1,51 @@
+package com.community_backend.springboot_api.models;
+
+import java.io.Serializable;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="TB_USERS")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    // id + generated value = bd vai criar os ids automaticamente
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String nome;
+    private String email;
+    private long nusp;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getNusp() {
+        return nusp;
+    }
+
+    public void setNusp(long nusp) {
+        this.nusp = nusp;
+    }
+}
